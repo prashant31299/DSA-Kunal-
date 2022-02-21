14bbb2cb382f3182cd690f69469c01b3166d4eac
prashantchavan <prashantchavan312199@gmail.com>
2022-02-21 11:05:20 +0530

    recurstion

--- recursion/findminmax.py
+++ recursion/findminmax.py
@@ -3,7 +3,9 @@
 def minrec(A,n):
     if (n==1):
         return A[0]
-    return min(A[n-1],minrec(A,n-1))
+    return max(A[n-1],minrec(A,n-1))
+    # this is search for min value 
+    #return min (A[n-1],minrec(A,n-1))
 
 A = [1, 4, 45, 6, -50, 10, 2]
 n=len(A)
---
+++ recursion/printn.py
@@ -0,0 +1,17 @@
+#print n to 1 
+"""def fun(n):
+    if (n==0):
+        return
+    
+    print(n)
+    fun(n-1)
+
+fun(5)"""
+
+# print 1 to n
+def reverse(n):
+    if n==0:
+        return
+    reverse(n-1)
+    print(n)
+reverse(5)
