a9e0e35d220515e23ca1142965ea02d72127a61b
prashantchavan <prashantchavan312199@gmail.com>
2022-02-24 08:23:29 +0530

    adding linear serch

---
+++ recursion/linear search.py
@@ -0,0 +1,18 @@
+def find(arr,t,i):
+    if (i==len(arr)):
+        return False
+    return arr[i]==t or find(arr,t,i+1)
+
+arr=[1,23,232,242,234,54,232,324,2]
+print(find(arr,23,0))
+
+# how to find the index
+def findindex(arr,t,i):
+    if (i==len(arr)):
+        return -1
+    if arr[i]==t:
+        return i 
+    else:
+        return findindex(arr,t,i+1)
+arr=[1,23,232,242,234,54,232,324,2]
+print(findindex(arr,324,0))
