ec9ea0473068054aaf721c367fed11107cd5e8cf
prashantchavan <prashantchavan312199@gmail.com>
2022-03-01 10:49:58 +0530

    return list

--- recursion/return list.py
+++ recursion/return list.py
@@ -1,13 +1,12 @@
-def findindexlist(arr,t,i):
-    list=[]
+def findindexlist(arr,t,i,list):
     if (i==len(arr)):
         return list
     if arr[i]==t:
         list.append(i)
     
-     return findindexlist(arr,t,i+1)
+    return findindexlist(arr,t,i+1,list)
 
 
 A=[]
 arr=[2,2,2,3,1,2,3,2]
-print(findindexlist(arr,3,0))
+print(findindexlist(arr,3,0,A))
